0cdfd587f0297a9c3c04f8c9535480f9fb683e2f
Murakami <murakamirc@gmail.com>
2014-03-06 10:33:41 -0300

    versao .ino

---
+++ demodulador/demodulador.ino
@@ -0,0 +1,11 @@
+/***********************************************************************************/
+/*                 AUTOBOTZ - ACADEMICS TEAM OF AUTONOMOS ROBOTICS                 */
+/*                   UNIVERSIDADE FEDERAL DE MINAS GERAIS - UFMG                   */
+/*                                                                                 */
+/*                       Autor Principal: Rafael Murakami                          */
+/*                                                                                 */
+/***********************************************************************************/
+
+/*Firmware desenvolvido com base no application note AVR135 e adaptado do codigo for-
+necido pela ATMEL de autoria de Bruce McKenny. Este firmware faz uso do Input Capture
+pin (ICP) do timer1 do atmega328P, que no arduino uno esta disponivel no pino 8.*/
